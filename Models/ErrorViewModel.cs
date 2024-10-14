b6eb461c940c166b6bc04bc5495140bb61b0408d
Boris <borisloncaric5@gmail.com>
2024-10-14 12:17:48 +0200

    Inicijalni commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lerin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
